c61986a4e13f5d7bcfa15b694b2087d0886d4572
KirthiRaman <kirthi.raman@gmail.com>
2014-12-05 09:55:01 -0500

    Create postfixEval.py

---
+++ algorithms/postfixEval.py
@@ -0,0 +1,33 @@
+import sys
+import stack
+
+def postfixEval(postfixExpr):
+    operandStack = stack.Stack()
+    tokenList = postfixExpr.split()
+
+    for token in tokenList:
+        if token in "0123456789":
+            operandStack.push(int(token))
+        else:
+            operand2 = operandStack.pop()
+            operand1 = operandStack.pop()
+            result = doMath(token,operand1,operand2)
+            operandStack.push(result)
+    return operandStack.pop()
+
+def doMath(op, op1, op2):
+    if op == "*":
+        return op1 * op2
+    elif op == "/":
+        return op1 / op2
+    elif op == "+":
+        return op1 + op2
+    else:
+        return op1 - op2
+
+
+def main(argv):
+    print(postfixEval('7 8 + 3 2 + /'))
+  
+if __name__ == "__main__":
+    main(sys.argv[1:])
