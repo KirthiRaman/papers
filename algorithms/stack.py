c38b313540b757d9a47ba8143ee52667a36bd482
KirthiRaman <kirthi.raman@gmail.com>
2014-12-05 09:14:59 -0500

    Update stack.py

--- algorithms/stack.py
+++ algorithms/stack.py
@@ -6,13 +6,13 @@ class Stack:
          return self.items == []
 
      def push(self, item):
-         self.items.append(item)
+         self.items.insert(0,item)
 
      def pop(self):
-         return self.items.pop()
+         return self.items.pop(0)
 
      def peek(self):
-         return self.items[len(self.items)-1]
+         return self.items[0]
 
      def size(self):
          return len(self.items)
